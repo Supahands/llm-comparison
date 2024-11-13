cc5caa227f99dce4973883ed15ff0e49b212dc49
EvanZJ <januar.evan2012@gmail.com>
2024-11-13 13:51:06 +0700

    add github repo button and star counter

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,14 @@ import * as animationData from "../public/animation/finish";
 import { supabaseClient } from "@/lib/supabase/supabaseClient";
 import { DATABASE_TABLE } from "@/lib/constants/databaseTables";
 
+import { FaGithub } from "react-icons/fa";
+import { IoStarOutline } from "react-icons/io5";
+
+import { useEffect, useState } from "react";
+import { set } from "date-fns";
+
 export default function Home() {
+  const [stars, setStars] = useState<string>("");
   const router = useRouter();
   const {
     sessionId,
@@ -75,6 +82,41 @@ export default function Home() {
     }
   };
 
+  function formatStarCount(count: number): string {
+    if (count >= 1000000) {
+      return (count / 1000000).toFixed(1) + "M";
+    } else if (count >= 1000) {
+      return (count / 1000).toFixed(1) + "K";
+    }
+    return count.toString();
+  }
+
+  const getStarsRepo = async (): Promise<void> => {
+    try {
+      const response = await fetch(
+        "https://api.github.com/repos/Supahands/llm-comparison-frontend",
+        {
+          headers: {
+            Authorization: `token ${process.env.NEXT_PUBLIC_TOKEN}`,
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error(`Failed to fetch: ${response.statusText}`);
+      }
+
+      const data = (await response.json()) as { stargazers_count: number };
+      setStars(formatStarCount(data.stargazers_count || 0));
+    } catch (error) {
+      console.error("Error fetching stars:", error);
+      setStars("0");
+    }
+  };
+
+  useEffect(() => {
+    getStarsRepo();
+  }, []);
+
   return (
     <div className="bg-llm-background h-full min-h-screen flex flex-col">
       <div className="flex flex-col mt-4">
@@ -86,7 +128,7 @@ export default function Home() {
           <section className="w-full lg:w-1/2 flex">
             <div className="flex flex-col w-full ">
               <Title>Select models to compare</Title>
-              <Card className="w-full h-full">
+              <Card className="w-full h-full rounded-xl">
                 <CardContent className="w-full h-full p-2">
                   <ModelSelector />
                 </CardContent>
@@ -97,14 +139,38 @@ export default function Home() {
       </div>
       <Comparison />
       <div className="flex flex-row w-full lg:justify-between justify-end mt-4 mb-4">
-        <LinkPreview
-          url="https://supa.so"
-          className="focus-visible:outline-llm-primary50 hidden lg:flex"
-        >
-          <Image src={`svg/logo.svg`} alt="SUPA logo" width={93} height={26} />
-        </LinkPreview>
+        <div className="flex gap-4">
+          <LinkPreview
+            url="https://supa.so"
+            className="focus-visible:outline-llm-primary50 hidden lg:flex"
+          >
+            <Image
+              src={`svg/logo.svg`}
+              alt="SUPA logo"
+              width={93}
+              height={26}
+            />
+          </LinkPreview>
+
+          <div
+            className="hidden lg:flex items-center font-extralight h-full group"
+            onClick={() => {
+              router.push(
+                "https://github.com/Supahands/llm-comparison-frontend"
+              );
+            }}
+          >
+            <div className="bg-white w-[40px] flex group-hover:bg-gray-300 items-center justify-center rounded-l-lg border-t border-l border-b h-full">
+              <FaGithub size={24} />
+            </div>
+            <div className="p-2 text-base font-semibold flex gap-[3px] justify-center items-center border-t border-r border-b bg-transparent group-hover:bg-gray-300 rounded-r-lg h-full">
+              <IoStarOutline /> {stars === "0" ? "Star" : stars}
+            </div>
+          </div>
+        </div>
+
         <Button
-          className="bg-llm-btn hover:bg-llm-btn_hover text-white rounded-2xl relative"
+          className="bg-llm-btn hover:bg-llm-btn_hover text-white rounded-xl relative"
           onClick={handleEvaluation}
           disabled={!selectedChoice}
         >
